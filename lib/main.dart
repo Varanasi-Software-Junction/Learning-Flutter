79cda31983acbf8cc06ebe76a8eaf71b80a109ce
champaksworldcreate <champaksworld@gmail.com>
2023-09-04 12:26:12 +0530

    The basic widget tree
    MaterialApp(
            title: 'Flutter layout demo',
            home: Scaffold(
              appBar: AppBar(
                title: Text('App Bar'),
              ),
              body: Text('Body'),
            ),
          ),
        );

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(
+      MaterialApp(
+        title: 'Flutter layout demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('App Bar'),
+          ),
+          body: Text('Body'),
+        ),
+      ),
+    );
