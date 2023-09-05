e4984ad3bf149e6bdbd6e3fb2c5ed2c5b5332a74
champaksworldcreate <champaksworld@gmail.com>
2023-09-05 19:03:33 +0530

    RichText
    RichText use.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Learning Flutter
  https://varanasisoftwarejunction.blogspot.com/2021/03/how-to-start-flutter-programming.html
+![img.png](img.png)
--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,24 @@ import 'package:flutter/material.dart';
 
 //https://api.flutter.dev/flutter/painting/TextStyle-class.html
 //https://api.flutter.dev/flutter/painting/BoxDecoration-class.html
+//https://api.flutter.dev/flutter/widgets/RichText-class.html
 void main() => runApp(
       MaterialApp(
         title: 'Text Style Demo',
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Text Style Demo'),
-          ),
-          body: Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.pink,
-                width: 2,
+            appBar: AppBar(
+              title: Text('Text Style Demo'),
+            ),
+            body: RichText(
+              text: TextSpan(
+                text: 'Varanasi ',
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Software',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: ' Junction!'),
+                ],
               ),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Text(
-              'Styling a text in Flutter',
-              style: TextStyle(fontSize: 32),
-            ),
-          ),
-        ),
+            )),
       ),
     );
