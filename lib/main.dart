2cb82d8c08ccb3dd20d4a079c718ee1c38befa57
champaksworldcreate <champaksworld@gmail.com>
2023-09-05 11:20:59 +0530

    Text Style Demo
    Text Style Demo using Box Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 //https://api.flutter.dev/flutter/painting/TextStyle-class.html
+//https://api.flutter.dev/flutter/painting/BoxDecoration-class.html
 void main() => runApp(
       MaterialApp(
         title: 'Text Style Demo',
@@ -8,14 +9,17 @@ void main() => runApp(
           appBar: AppBar(
             title: Text('Text Style Demo'),
           ),
-          body: Text(
-            'This is the body of the App.\n Developed by Varanasi Software Junction.',
-            style: TextStyle(
-              fontStyle: FontStyle.italic,
-              color: Colors.deepOrange,
-              backgroundColor: Colors.green,
-              fontFamily: 'Arial',
-              fontSize: 50,
+          body: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.pink,
+                width: 2,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              'Styling a text in Flutter',
+              style: TextStyle(fontSize: 32),
             ),
           ),
         ),
