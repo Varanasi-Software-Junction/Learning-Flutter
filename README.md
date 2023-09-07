e67ce8015a3d5e5cab93827a2b40ac8aea91cbfc
champaksworldcreate <champaksworld@gmail.com>
2023-09-07 08:30:23 +0530

    A simple Login Form
    Made a simple login form

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Learning Flutter
  https://varanasisoftwarejunction.blogspot.com/2021/03/how-to-start-flutter-programming.html
-![img_1.png](img_1.png)
+![img_2.png](img_2.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,65 @@
 import 'package:flutter/material.dart';
 
-//https://docs.flutter.dev/cookbook/forms/text-input
-void main() => runApp(
-      MaterialApp(
-        title: 'Text Field Demo',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Text Field Demo'),
-          ),
-          body: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Type something',
-            ),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Form',
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Form'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+              SizedBox(height: 32.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    child: Text('Login'),
+                  ),
+                  SizedBox(width: 16.0),
+                  TextButton(
+                    onPressed: () {
+
+                    },
+                    child: Text('Forgot Password?'),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+}
